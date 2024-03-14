c6e9103a9f9d3eb1a04f232dbeca3fb887e27b58
MatteoGiunta97 <matteogiunta@outlook.it>
2024-03-14 17:10:56 +0100

    feat: add palWord function, palindroma

--- palindroma/js/script.js
+++ palindroma/js/script.js
@@ -4,3 +4,31 @@ Chiedere all’utente di inserire una parola
 Creare una funzione per capire se la parola inserita è palindroma
 Su internet ci sono soluzioni con join e reverse, non potete usarle!
 */
+
+
+
+// Chiedo all'utente di inserire una parola e la passo nella funzione
+const userWord = prompt('inserire una parola');
+
+palWord(userWord);
+
+
+// Creo una funzione per capire se la parola è palindroma
+    function palWord(word) {
+
+        let lgth = Math.floor(word.length / 2);
+        
+        for (let index = 0; index < lgth; index++)
+
+        if (word[index] !== word[word.length - index - 1]) {
+
+            const message = alert('La parola inserita NON è palindroma')
+            return message;
+
+        }else {
+
+            const message = alert('La parola inserita è palindroma')
+            return message;
+        }
+
+    }
