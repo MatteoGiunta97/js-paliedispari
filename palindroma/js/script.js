1f7db7a32b849ab0992e58a5dfb3283b8dc0912f
MatteoGiunta97 <matteogiunta@outlook.it>
2024-03-14 17:49:39 +0100

    feat: add comments palWord function, palindroma

--- palindroma/js/script.js
+++ palindroma/js/script.js
@@ -15,20 +15,26 @@ palWord(userWord);
 
 // Creo una funzione per capire se la parola è palindroma
     function palWord(word) {
+        // Se le lettere della prima metà non corrispondondono alla seconda metà, 
+        // allora la parola non è palindroma, altrimenti lo è
 
+        // Divido la lunghezza della parola in 2
         let lgth = Math.floor(word.length / 2);
-        
+
+        // Passo le lettere in un ciclo che si interrompe a metà parola
         for (let index = 0; index < lgth; index++)
 
-        if (word[index] !== word[word.length - index - 1]) {
+            // Confronto la lettera della prima metà (nella posizione index attuale del ciclo) con la lettera della seconda metà (nella posizione index opposta)
+            if (word[index] !== word[word.length - index - 1]) {
 
+            // Stampo il risultato    
             const message = alert('La parola inserita NON è palindroma')
             return message;
 
-        }else {
+            }else {
 
             const message = alert('La parola inserita è palindroma')
             return message;
-        }
+            }
 
     }
