b78d961c4449c83e0ce7a92344469024d1f50456
MatteoGiunta97 <matteogiunta@outlook.it>
2024-03-14 14:47:49 +0100

    feat: add comments, pari e dispari

--- pari-e-dispari/js/script.js
+++ pari-e-dispari/js/script.js
@@ -6,3 +6,15 @@ Sommiamo i due numeri
 Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 Dichiariamo chi ha vinto.
 */
+
+
+
+// Chiedo all'utente di inserire pari o dispari
+
+// Chiedo all'utente di inserire un numero da 1 a 5
+
+// Genero un numero random da 1 a 5 per il computer
+
+// Sommo i due numeri e verifico se la somma è pari o dispari
+
+// Dichiaro il vincitore
