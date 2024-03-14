d62bb0c184ee8731ed57b7ad27eb26328dd4f0c9
MatteoGiunta97 <matteogiunta@outlook.it>
2024-03-14 15:24:31 +0100

    feat: add numRandom function, pari e dispari

--- pari-e-dispari/js/script.js
+++ pari-e-dispari/js/script.js
@@ -1,5 +1,5 @@
 /*
-Pari e Dispari
+Pari e Dispari:
 L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
 Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
 Sommiamo i due numeri
@@ -16,8 +16,19 @@ const evenOrOdd = prompt('inserisci la parola "pari" o "dispari"');
 const userNum = parseInt(prompt('inserisci un numero da 1 a 5'));
 
 // Genero un numero random da 1 a 5 per il computer
-
+const compNum = numRandom(1,5);
+console.log(compNum)
 
 // Sommo i due numeri e verifico se la somma è pari o dispari
 
 // Dichiaro il vincitore
+
+
+/*
+ FUNZIONI
+*/
+
+// La funzione genera un numero intero random tra due parametri: min e max
+function numRandom(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
